0bf253ea63109decd0b1528b8d5dad347bbe08cf
tomact116 <tomact116@gmail.com>
2026-02-04 22:49:33 +0300

    Create method index()

--- Django/mysite/firstsite/firstsite/urls.py
+++ Django/mysite/firstsite/firstsite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sportsmen.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sportsmen/', index),
 ]
