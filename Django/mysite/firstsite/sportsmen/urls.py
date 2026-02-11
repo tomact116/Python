7127463e35016f70f70bdbcfd8bf3c4ef71a0d33
tomact116 <tomact116@gmail.com>
2026-02-11 15:28:50 +0300

    path  home

--- Django/mysite/firstsite/sportsmen/urls.py
+++ Django/mysite/firstsite/sportsmen/urls.py
@@ -6,7 +6,8 @@ from sportsmen.views import index, sports
 #直接把所有应用的URL都放在主 urls.py 中不是好方法。我们应该为每个应用创建自己的 urls.py
 
 urlpatterns = [
-    path('', index),          # 对应 /sportsmen/
+    #path('', index),          # 对应 /sportsmen/
+    path('', index, name='home'), 
     path('sports/<int:sp_id>/', sports)
     #path('sports/', sports)   # 对应 /sportsmen/sports/
     #re_path(r'^sports/(?P<year>[0-9]{4})/$', sports),
