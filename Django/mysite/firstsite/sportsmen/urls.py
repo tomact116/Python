820e60acef1ca8e192cdb24391edd053cd086a2a
tomact116 <tomact116@gmail.com>
2026-02-11 11:24:24 +0300

    re_path()  URL

--- Django/mysite/firstsite/sportsmen/urls.py
+++ Django/mysite/firstsite/sportsmen/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+#from django.urls import path
+from django.urls import path, re_path  # 导入re_path
 #from . import views  # 导入当前应用的views
 from sportsmen.views import index, sports
 
@@ -6,5 +7,7 @@ from sportsmen.views import index, sports
 
 urlpatterns = [
     path('', index),          # 对应 /sportsmen/
-    path('sports/<int:sp_id>/', sports)   # 对应 /sportsmen/sports/
+    #path('sports/<int:sp_id>/', sports)   # 对应 /sportsmen/sports/
+    #path('sports/', sports)   # 对应 /sportsmen/sports/
+    re_path(r'^sports/(?P<year>[0-9]{4})/$', sports),
 ]
--- Django/mysite/firstsite/sportsmen/views.py
+++ Django/mysite/firstsite/sportsmen/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Welcome to the sportsmen website!")
 
-def sports(request, sp_id):
-    return HttpResponse(f"<h1>Articles by sports</h1><p>Sports ID: {sp_id}</p>")
+def sports(request, year):
+    return HttpResponse(f"<h1>Articles by sports</h1><p>Sports ID: {year}</p>")
