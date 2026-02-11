fedc8f3420327c296cfa6bed3b7ad1618859b028
tomact116 <tomact116@gmail.com>
2026-02-11 15:23:56 +0300

    Http404 redirect  HttpResponsePermanentRedirect

--- Django/mysite/firstsite/firstsite/settings.py
+++ Django/mysite/firstsite/firstsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+70tt4a!4pr#2048_g8^ndr#(yj$^#r=x5ghlnq&bj6wy*wdh5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True #False  True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
--- Django/mysite/firstsite/sportsmen/views.py
+++ Django/mysite/firstsite/sportsmen/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+#from django.http import HttpResponse, HttpResponseNotFound
+from django.http import Http404
+from django.shortcuts import redirect
+from django.http import HttpResponsePermanentRedirect
 
 # Create your views here.
 from django.http import HttpResponse
@@ -7,10 +11,19 @@ def index(request):
     return HttpResponse("Welcome to the sportsmen website!")
 
 def sports(request, sp_id):
-    print(request.GET)
+    if(request.GET):
+        print(request.GET)
+
+    if(sp_id>10):
+        #raise Http404()
+        #return redirect('/')
+        #return redirect(to='/', permanent=True)
+        return HttpResponsePermanentRedirect('/')
+
+
     return HttpResponse(f"<h1>Articles by sports</h1><p>Sports ID: {sp_id}</p>")
 
-from django.http import HttpResponse, HttpResponseNotFound
+
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound("<h1>404 - Page Not Found</h1>")
