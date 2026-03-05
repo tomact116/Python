97cdce60ad8ff4cb37cfd9b40768fb68d077ccf1
tomact116 <tomact116@gmail.com>
2026-03-05 17:25:33 +0300

    Practical 8 THE ADMIN PANEL

--- Django/mysite/firstsite/sportsmen/apps.py
+++ Django/mysite/firstsite/sportsmen/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class SportsmenConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'sportsmen'
+    verbose_name = 'News'
--- Django/mysite/firstsite/sportsmen/models.py
+++ Django/mysite/firstsite/sportsmen/models.py
@@ -3,26 +3,34 @@ from django.urls import reverse
 
 # Create your models here.
 class Sportsman(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    sport = models.ForeignKey('Sports', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name='Article title')
+    content = models.TextField(blank=True, verbose_name='Article content')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Photo')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Time of creation')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Time of last modification')
+    is_published = models.BooleanField(default=True, verbose_name='Publication')
+    sport = models.ForeignKey('Sports', on_delete=models.PROTECT, null=True, verbose_name='Sports')
 	
     def __str__(self):
       return self.title
 
     def get_absolute_url(self):
       return reverse('post', kwargs={'post_id': self.pk})
-
+      
+    class Meta:
+        verbose_name = 'Sports news'
+        verbose_name_plural = 'Sports news'
+        ordering = ['time_create', 'title']   # 先按创建时间，再按标题排序
 
 class Sports(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='The name of the sport')
 
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
+    def get_absolute_url(self):#没有
         return reverse('sport', kwargs={'sport_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Sport'
+        ordering = ['name']
